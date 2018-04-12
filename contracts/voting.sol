c1518372a92469f8b18c6805dd9c0dc5910c1c20
Nikhil Singh <Singh.nikhil999@gmail.com>
2018-04-12 15:14:52 +0530

    minor changes :tada:

--- contracts/voting.sol
+++ contracts/voting.sol
@@ -12,4 +12,32 @@ contract Voting {
   }
 }
 
-function getResults (bytes32 candidate)
+function getResults (bytes32 candidate) public{
+  // necessary for d
+  require(ifValidCandidate(candidate));
+  require(ifValidVote(msg.sender));
+
+  votesRecieved[candidate] += 1;
+  voters.push(msg.sender)
+
+}
+
+function ifValidCandidate(bytes32 candidate) view public returns (bool) {
+
+    for(uint i = 0; i < candidates.length; i++) {
+      if (candidates[i] == candidate) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function ifValidVoter(address voter) view public returns (bool) {
+    for(uint i = 0; i < voters.length; i++) {
+      if (voters[i] == voter) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
