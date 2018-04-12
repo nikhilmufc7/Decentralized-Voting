2de13e6f8b8479f40f26220d73e2ebce47520636
Nikhil Singh <Singh.nikhil999@gmail.com>
2018-04-12 15:59:33 +0530

    added pug files :zap:

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,102 +1,43 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+<template lang="pug">
+  div
+    section.hero.is-dark
+      .hero-body
+        .container
+          h1.title General Elections in 2018
+          h2.subtitle
+            | Select your preferred candidate&nbsp;
+            strong preferred
+            | .
+    section.section
+      .container
+        .columns
+          .column(v-for='(candidate, index) in candidates', @click='vote(candidate.name)')
+            .candidate-card
+              img(:src='candidate.photo')
+              .candadite-data
+                .candidate-name \#{{index+1}} {{candidate.name}}
+                div
+                  span(style='font-weight:bold') Occupation:&nbsp;
+                  span {{candidate.occupation}}
+              div(:class='candidate.style')
+                | Vote here
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App"
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
