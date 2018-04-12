1c9391eb0a9a4ab44875e198ea2ffcfcfb96ab0b
Nikhil Singh <Singh.nikhil999@gmail.com>
2018-04-12 11:55:06 +0530

    fixes typo

--- contracts/voting.sol
+++ contracts/voting.sol
@@ -1,10 +1,13 @@
 pragma solidity ^0.4.18;
 
-contract voting{
+contract Voting {
   bytes32[] private candidate;
 
   address[] private voters;
 
   mapping (bytes32 => uint8) private votesRecieved;
 
+  function Voting(bytes32[] candidateNames) public {
+    candidates = candidateNames;
+  }
 }
